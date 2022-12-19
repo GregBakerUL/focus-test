ec3613e667cfe708341580325d55d0d8de48a6f3
Gregory Baker <gregory.baker1993@gmail.com>
2022-12-19 14:47:49 +0000

    works on 1 sensor images

--- analyze_sharpness.py
+++ analyze_sharpness.py
@@ -1,3 +1,4 @@
+#%%
 # import the necessary packages
 from imutils import paths
 import argparse
@@ -8,8 +9,8 @@ import numpy as np
 import seaborn as sns
 import statistics
 import pandas
-import sys
-import csv
+import os
+
 
 # compute the Laplacian of the image and then return the focus
 # measure, which is simply the variance of the Laplacian
@@ -18,12 +19,11 @@ def variance_of_laplacian(image):
 	return cv2.Laplacian(image, cv2.CV_64F).var()
 
 
-# [1] Read image
-# [2] Convert to grayscale
-# [3] Replace embedded line row with previous row
-# [4] Split image into left and right
-# [5] Calculate laplacian for both images
-# [6] Return results
+def find_files_in_dir( path_to_dir, suffix=".png"):
+    filenames = os.listdir(path_to_dir)
+    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+
 def process_side_by_side_image(image_path):
     
     image = cv2.imread(image_path)                                      # [1]
@@ -39,11 +39,8 @@ def process_side_by_side_image(image_path):
 
 def display_image(image_path, focus_metric, label):
     image = cv2.imread(image_path) 
-    cv2.putText(image, "LapVar: {:.2f}".format(focus_metric[0]), (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.8, 255, 3)
-    cv2.putText(image, "LapVar: {:.2f}".format(focus_metric[1]), (522, 30),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.8, 255, 3)
-    
+    cv2.putText(image, "LapVar: {:.2f}".format(focus_metric), (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.8, 255, 3)    
     cv2.imshow(label, image)
     
     key = cv2.waitKey(0)
@@ -58,76 +55,69 @@ def main():
         help="focus measures that fall below this value will be considered 'blurry'")
     args = vars(ap.parse_args())
 
-    side_label = ["left", "right"]
+    side_label = ["L", "R"]
     labels= []
-    fm_av = [],[],[]
-    fm_sensor_left = []
-    fm_sensor_right = []
+    fm_av = []
+    fm_image = []
+    counter = 0
+    id = 1
+    counters = []
     
     display_images = False
-    
 
     for image_path in paths.list_images(args["images"]):
-        
-        image_path_split = image_path.split('\\')
-        if not (image_path_split[9] == 'Assembled' and image_path_split[10] == 'dome_focus'):
-            continue
-        
-        label = image_path.split('\\')[8].split('FFFF0000')[1]
-        if label not in labels:
+        image_name = image_path.split('\\')[-1]
+        serial = image_name.split('.')[0].split('_')[2] + '_' + image_name.split('.')[0].split('_')[3]
+        if serial not in labels:
             if len(labels) > 0:
-                fm_av_sensor_left = statistics.fmean(fm_sensor_left)
-                fm_av_sensor_right = statistics.fmean(fm_sensor_right)
-                fm_av[0].append(fm_av_sensor_left)
-                fm_av[1].append(fm_av_sensor_right)
-                fm_av[2].append((fm_av_sensor_left + fm_av_sensor_right)/2)
+                fm_av.append(statistics.fmean(fm_image))
                 
-            print(label)
-            labels.append(label)
-            fm_sensor_left = []
-            fm_sensor_right = []
-        
-        fm = process_side_by_side_image(image_path)
-        if display_images and max(fm) < args["threshold"]:
-            display_image(image_path, fm, label)
-        fm_sensor_left.append(fm[0])
-        fm_sensor_right.append(fm[1])
+            print(serial)
+            labels.append(serial)
 
-    fm_av_sensor_left = statistics.fmean(fm_sensor_left)
-    fm_av_sensor_right = statistics.fmean(fm_sensor_right)
-    fm_av[0].append(fm_av_sensor_left)
-    fm_av[1].append(fm_av_sensor_right)
-    fm_av[2].append((fm_av_sensor_left + fm_av_sensor_right)/2)
+            # check if counter is odd or even
+            side = side_label[counter % 2]
+            if counter % 2 == 1:
+                id += 1
+            counters.append(str(id) + '_' + side)
+            counter += 1
+            fm_image = []
+        
+        image = cv2.imread(image_path)
+        fm = variance_of_laplacian(image)
+        
+        if display_images and fm < args["threshold"]:
+            display_image(image_path, fm, serial)
+        fm_image.append(fm)
+
+    fm_av.append(statistics.fmean(fm_image))
     
     fm_av_df = pandas.DataFrame(fm_av)
-    fm_av_df = fm_av_df.transpose()
-    fm_av_df.index = labels
-    fm_av_df.columns = ['fm_left', 'fm_right', 'fm_average']
-    fm_av_df.sort_values(by=fm_av_df.columns[2], inplace=True)
+    # fm_av_df = fm_av_df.transpose()
+    fm_av_df.index = counters
+    fm_av_df.sort_values(by=fm_av_df.columns[0], inplace=True)
     print(fm_av_df.head())
+    
 
-    x_scatter = np.linspace(1, len(fm_av[0]), len(fm_av[0]))
+    x_scatter = np.linspace(1, len(fm_av_df), len(fm_av_df))
 
-    fig, ax = plt.subplots(1, 1, figsize=(10,9))
+    fig, ax = plt.subplots(1, 1, figsize=(15,9))
     plt.xlabel('Sensor')
     plt.ylabel('Focus [a.u.]')
     plt.title('Sensor focus level')
-    plt.xticks(x_scatter, fm_av_df.index)
-    plt.xticks(rotation=90)
-    ax.scatter(x_scatter, fm_av_df['fm_left'], s=8, label= "Left")
-    ax.scatter(x_scatter, fm_av_df['fm_right'], s=8, label="Right")
-    ax.scatter(x_scatter, fm_av_df['fm_average'], s=8, label="Average", color='lightgray')
+    plt.xticks(x_scatter, fm_av_df.index, rotation=90)
+    ax.scatter(x_scatter, fm_av_df, s=8)
     ax.legend()
     ax.grid(True)
     fig.savefig("plots/scatter.png")
     
     
     fig1, ax1 = plt.subplots()
-    sns.kdeplot(data=fm_av[0], ax=ax1, label="Left")
-    sns.kdeplot(data=fm_av[1], ax=ax1, label="Right")
+    sns.kdeplot(data=fm_av, ax=ax1, label="Focus")
     ax1.legend()
     fig1.savefig("plots/distribution.png")
     plt.show()
     
 if __name__ == "__main__":
     main()
+# %%
